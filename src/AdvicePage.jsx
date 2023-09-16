ddc05067fa317d67974b73f19c7ca947e2d0881a
Fola-Joe <104533211+Fola-Joe@users.noreply.github.com>
2023-09-16 03:04:13 +0100

    fix images

--- src/AdvicePage.jsx
+++ src/AdvicePage.jsx
@@ -23,12 +23,12 @@ export default function AdvicePage() {
                 <h1 className="h1">ADVICE #{quote.id}</h1>
                 <q className="advice">“{quote.advice}”</q>
                 <picture>
-                    <source media="(min-width: 750px)" srcSet="./src/images/pattern-divider-desktop.svg" />
-                    <img src="./src/images/pattern-divider-mobile.svg" alt="pattern divider image" />
+                    <source media="(min-width: 750px)" srcSet="./pattern-divider-desktop.svg" />
+                    <img src="./pattern-divider-mobile.svg" alt="pattern divider image" />
                 </picture>
             </section>
             <div className="Dice" onClick={GetQuote} >
-                <img src="./src/images/icon-dice.svg" alt="dice icon"/>
+                <img src="./icon-dice.svg" alt="dice icon"/>
             </div>
         </div>
     )
